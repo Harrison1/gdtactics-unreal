a893d0bbbce8d4343058c60e6c7ba6f913443cd0
Harrison <hmcguire13@gmail.com>
2025-07-05 19:47:14 -0400

    Updated gdtactics gym

---
+++ Source/MyProject/ActivationPad/ActivationPad.cpp
@@ -0,0 +1,69 @@
+﻿#include "ActivationPad.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/GameplayStatics.h"
+#include "MyProject/Prototype/PrototypeInterface.h"
+#include "NiagaraSystem.h"
+#include "NiagaraFunctionLibrary.h"
+
+AActivationPad::AActivationPad()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Base = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Base"));
+	RootComponent = Base;
+
+	Pad = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Pad"));
+	Pad->SetupAttachment(RootComponent);
+
+	BoxComponent = CreateDefaultSubobject<UBoxComponent>(TEXT("BoxComponent"));
+	BoxComponent->SetupAttachment(RootComponent);
+}
+
+void AActivationPad::StartAction()
+{
+	if (InterfaceActors.IsEmpty())
+	{
+		TArray<AActor*> OverlappingActors;
+		BoxComponent->GetOverlappingActors(OverlappingActors, AActor::StaticClass());
+
+		for (AActor* Actor : OverlappingActors)
+		{
+			if (Actor->Implements<UPrototypeInterface>())
+			{
+				InterfaceActors.AddUnique(Actor);
+			}
+		}
+	}
+
+	for (AActor* Actor : InterfaceActors)
+	{
+		IPrototypeInterface::Execute_Interact(Actor);
+	}
+
+	if (PressureSound != nullptr)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, PressureSound, GetActorLocation());
+	}
+
+	if (Steam != nullptr)
+	{
+		FFXSystemSpawnParameters SpawnParams;
+		SpawnParams.WorldContextObject = this;
+		SpawnParams.SystemTemplate = Steam;
+		SpawnParams.Location = GetActorLocation();
+		SpawnParams.Rotation = GetActorRotation();
+		SpawnParams.Scale = FVector(2.f, 2.f, 1.f);
+		SpawnParams.bAutoDestroy = true;
+		SpawnParams.bAutoActivate = true;
+		
+		UNiagaraFunctionLibrary::SpawnSystemAtLocationWithParams(SpawnParams);
+	}
+}
+
+void AActivationPad::StopAction()
+{
+	for (AActor* Actor : InterfaceActors)
+	{
+		IPrototypeInterface::Execute_StopInteract(Actor);
+	}
+}
---
+++ Source/MyProject/ActivationPad/ActivationPad.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "ActivationPad.generated.h"
+
+class UBoxComponent;
+class UNiagaraSystem;
+
+UCLASS()
+class MYPROJECT_API AActivationPad : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AActivationPad();
+	
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* Base;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UStaticMeshComponent* Pad;
+
+	UPROPERTY(EditAnywhere)
+	UBoxComponent* BoxComponent;
+
+	UPROPERTY(EditAnywhere)
+	TArray<AActor*> InterfaceActors;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	USoundBase* PressureSound;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	UNiagaraSystem* Steam;
+
+	UFUNCTION(BlueprintCallable)
+	void StartAction();
+
+	UFUNCTION(BlueprintCallable)
+	void StopAction();
+};
